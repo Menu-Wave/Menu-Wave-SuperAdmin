6af691367258cd20e234ed190b096676e1d46c78
eradelaleinad369-sudo <eradelaleinad369@gmail.com>
2026-07-15 00:32:34 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://xyzxvqcezhthphrvtmuo.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpiaGxmbHhmdmVmZ3ViYmp1ZGFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MzQxODksImV4cCI6MjA5ODQxMDE4OX0.zzYSmhQyA4rj0q_LQL-tI8e4VxAjQKZPwgR4heOx45k";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5enh2cWNlemh0aHBocnZ0bXVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwNTU2ODAsImV4cCI6MjA5OTYzMTY4MH0.DyhMs63nhYwwbd8Ezb0_Hr3d-2c9sTeZ6phLe-Cf5hg";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
